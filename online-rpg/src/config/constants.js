5af85a9589dab9b57607ac5d9155bd8cf76f8654
mogwai <feelirabs@gmail.com>
2020-03-04 05:49:53 +0100

    Add dynamic movement to character

--- online-rpg/src/config/constants.js
+++ online-rpg/src/config/constants.js
@@ -1,3 +1,4 @@
-export const SPRITE_SIZE = 40
-export const MAP_HEIGHT = SPRITE_SIZE * 10
-export const MAP_WIDTH = SPRITE_SIZE * 20
+export const SPRITE_SIZE = 32
+export const TILE_SIZE = 50
+export const MAP_HEIGHT = TILE_SIZE * 10
+export const MAP_WIDTH = TILE_SIZE * 20
--- online-rpg/src/features/player/index.js
+++ online-rpg/src/features/player/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux'
 // import walkSprite from './player_walk.png'
 import handleMovement from './movement'
+import {SPRITE_SIZE} from '../../config/constants'
+import useEventListener from '@use-it/event-listener'
+
+const DIRECTION = {
+    DOWN: 0,
+    LEFT: SPRITE_SIZE * 1,
+    RIGHT: SPRITE_SIZE * 2,
+    UP: SPRITE_SIZE * 3
+}
+
 function Player(props) {
     const offset = {top:0, left:0}
+    const [facing, setFacing] = useState(DIRECTION.DOWN)
+
+    useEventListener("keydown", ({ code })=> {
+        if(code.indexOf("Arrow")==-1) return
+        const direction = DIRECTION[code.replace("Arrow", "").toUpperCase()]
+        setFacing(direction)
+
+    })
     return (
         <div
         style = {{
             position: 'absolute',
             top: props.position[1],
             left: props.position[0],
-            background: `url(/player_walk.png) -${offset.left}px -${offset.top}px`,
+            background: `url(/player_walk.png) -${offset.left}px -${facing}px`,
             // backgroundPosition: '0 96',
-            width: '32px',
-            height: '32px'
+            width: SPRITE_SIZE,
+            height:SPRITE_SIZE,
         }}
         />
     )
